b9d0c22c06f47a0f1bbdfb5cf0561e452ca69981
Kirill Kirsanov <montego@yandex.ru>
2023-11-09 22:43:02 +0300

    Added functionality for graphql creating entities

--- main/schema.py
+++ main/schema.py
@@ -1,6 +1,16 @@
+from email import message
+from django.db.migrations import serializer
 import graphene
+from graphene.types.scalars import Scalar
 from graphene_django import DjangoObjectType
+from graphene_django.rest_framework.mutation import SerializerMutation
+from main.serializers import CarPassSerializer
 from .models import CarPass
+
+class ObjectField(Scalar): 
+    @staticmethod
+    def serialize(dt):
+        return dt 
 
 class CarPassType(DjangoObjectType):
     class Meta:
@@ -18,8 +28,40 @@ class Query(graphene.ObjectType):
         return CarPass.objects.get(pk=id)
     
 
-class CarCreateMutation(graphene.Mutation):
-    
+class CreateCar(graphene.Mutation):
+    car_pass = graphene.Field(CarPassType)
+    message = ObjectField()
+    status = graphene.Int()
+
+    class Arguments:
+        brand = graphene.String(required=True)
+        model = graphene.String(required=True)
+        plate_number = graphene.String(required=True)
+        owners_name = graphene.String(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        serializer = CarPassSerializer(data=kwargs)
+        if serializer.is_valid():
+            obj=serializer.save()
+            msg='success'
+            status = 201
+        else:
+            msg=serializer.errors
+            obj=None
+            status = 400
+        return cls(car_pass=obj,message=msg,status=status)
 
 
-schema = graphene.Schema(query=Query)
+# class CarCreateUpdateMutation(SerializerMutation):
+#     class Meta:
+#         serializer_class = CarPassSerializer
+#         model_operations = ['create', 'update']
+#         lookup_field = 'id'
+
+class Mutation(graphene.ObjectType):
+    create_car = CreateCar.Field()
+    # update_car = CarCreateUpdateMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
