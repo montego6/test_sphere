8c6e31b6f5cae44c159e4df8f7179eb59ee75f89
Kirill Kirsanov <montego@yandex.ru>
2023-11-09 18:40:00 +0300

    Added carpass model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CarPass
+
+admin.site.register(CarPass)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
+class CarPass(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand = models.CharField(max_length=48)
+    model = models.CharField(max_length=48)
+    plate_number = models.CharField(max_length=24)
+    owners_name = models.CharField(max_length=96)
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
