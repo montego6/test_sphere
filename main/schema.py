9ca4bf0c73b812295f0964963528a3d58e5d459c
Kirill Kirsanov <montego@yandex.ru>
2023-11-09 22:47:47 +0300

    Added graphql update functionality

--- main/schema.py
+++ main/schema.py
@@ -53,15 +53,36 @@ class CreateCar(graphene.Mutation):
         return cls(car_pass=obj,message=msg,status=status)
 
 
-# class CarCreateUpdateMutation(SerializerMutation):
-#     class Meta:
-#         serializer_class = CarPassSerializer
-#         model_operations = ['create', 'update']
-#         lookup_field = 'id'
+class UpdateCar(graphene.Mutation):
+    car_pass = graphene.Field(CarPassType)
+    message = ObjectField()
+    status = graphene.Int()
+
+    class Arguments:
+        id = graphene.ID(required=True)
+        brand = graphene.String()
+        model = graphene.String()
+        plate_number = graphene.String()
+        owners_name = graphene.String()
+
+    @classmethod
+    def mutate(cls, root, info, id, **kwargs):
+        car = CarPass.objects.get(id=id)
+        serializer = CarPassSerializer(car, data=kwargs, partial=True)
+        if serializer.is_valid():
+            obj=serializer.save()
+            msg='success'
+            status = 201
+        else:
+            msg=serializer.errors
+            obj=None
+            status = 400
+        return cls(car_pass=obj,message=msg,status=status)
+
 
 class Mutation(graphene.ObjectType):
     create_car = CreateCar.Field()
-    # update_car = CarCreateUpdateMutation.Field()
+    update_car = UpdateCar.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
